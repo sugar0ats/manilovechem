eb1cff579e62047fc8f3996da8960df1439ddc1a
emma <emmacao1234@gmail.com>
2022-07-16 22:42:58 -0700

    neutron and proton count working!!

--- Assets/Scripts/NucleusBehavior.cs
+++ Assets/Scripts/NucleusBehavior.cs
@@ -3,22 +3,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class NucleusBehavior : MonoBehaviour
 {
     public GameObject protonField;
-    public InputField protonInput;
+    public TMP_InputField protonInput;
 
     public GameObject neutronField;
-    public InputField neutronInput;
+    public TMP_InputField neutronInput;
 
     public int protonCount;
     public int neutronCount;
     // Start is called before the first frame update
     void Start()
     {
-        protonInput = protonField.GetComponent<InputField>();
-        neutronInput = neutronField.GetComponent<InputField>();
+        //Debug.Log("doesthis work?");
+        
+        updateProtons();
+        updateNeutrons();
+        // protonInput = protonField.GetComponent<TMP_InputField>();
+        // neutronInput = neutronField.GetComponent<TMP_InputField>();
+        protonInput.text = "0";
+        neutronInput.text = "0";
     }
 
     // Update is called once per frame
@@ -29,13 +36,20 @@ public class NucleusBehavior : MonoBehaviour
 
     public void updateProtons()
     {
-        //InputField protonNums = protonField.GetComponent<InputField>();
+
         protonCount = (protonInput.text != "" ? Int16.Parse(protonInput.text) : 0);
 
     }
 
     public void updateNeutrons()
     {
+
         neutronCount = (neutronInput.text != "" ? Int16.Parse(neutronInput.text) : 0);
+    }
+
+    public void repZeroPro() {
+        if (protonInput.text == "") {
+
+        }
     }
 }
